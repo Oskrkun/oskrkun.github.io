1c09e0305e1a9820842feb743eb8560f296e14f3
Oscar Charlo <ocharlo2024@gmail.com>
2025-03-08 00:11:01 -0300

    Update controlProductos.js

--- js/controlProductos.js
+++ js/controlProductos.js
@@ -53,19 +53,9 @@ export async function cargarTiposLentes() {
                 tipoLentesContainer.appendChild(row);
             });
 
-            // Agregar evento para permitir deseleccionar
+            // Agregar evento para permitir solo un checkbox seleccionado
             tipoLentesContainer.querySelectorAll('input[type="radio"]').forEach(radio => {
-                radio.addEventListener('click', function(event) {
-                    // Desmarcar si se vuelve a hacer clic en el mismo
-                    if (this.checked) {
-                        this.checked = false;
-                        // Llama a la función para cargar productos filtrados
-                        cargarProductosFiltrados();
-                    } else {
-                        // Si se selecciona un nuevo radio, carga los productos
-                        cargarProductosFiltrados();
-                    }
-                });
+                radio.addEventListener('change', cargarProductosFiltrados);
             });
         } else {
             console.error('Contenedor de tipos de lentes no encontrado.');
