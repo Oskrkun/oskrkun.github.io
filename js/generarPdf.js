6bd48bc5564c9d189e1335ae77cde23d9724a0cf
Oscar Charlo <ocharlo2024@gmail.com>
2025-03-09 17:53:36 -0300

    Update generarPdf.js

--- js/generarPdf.js
+++ js/generarPdf.js
@@ -78,7 +78,7 @@ export function generarPDF() {
 				margin: [5, 10, 10, 10], // [top, right, bottom, left]
 				filename: `Presupuesto_${cliente}.pdf`,
 				image: { type: 'jpeg', quality: 0.98 },
-				html2canvas: { scale: 2, useCORS: true },
+				html2canvas: { scale: 1, useCORS: true },
 				jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
 			};
 
