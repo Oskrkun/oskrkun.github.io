03adc3b0e110ae200d89ab393c3d27ae7ff8f98e
Oscar Charlo <ocharlo2024@gmail.com>
2025-03-14 15:14:22 -0300

    Update presupuesto.js

--- js/presupuesto.js
+++ js/presupuesto.js
@@ -14,7 +14,7 @@ import {
     mostrarAdvertenciaMaxEsfCil,
     calcularCerca,
     sincronizarCambios,
-    agregarEventosSincronizacion,
+    agregarEventosDelegacion,
     esEsfOCil,
     transponerReceta,
     sincronizarTodo,
